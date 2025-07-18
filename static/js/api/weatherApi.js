88c417dbf2481346867d7b4ed9b9b4e985e0f03d
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2025-07-18 02:23:08 -0300

    actualizacion url base para la api

--- static/js/api/weatherApi.js
+++ static/js/api/weatherApi.js
@@ -1,6 +1,6 @@
 // la url base para la api.
 import { handleApiError, handleRequestError } from "../utilsjs/errorHandler.js";
-const api_base_url = 'https://app-clima-cfrn3z15t-daianas-projects-12659579.vercel.app' 
+const api_base_url = 'https://app-clima-zshg.onrender.com' 
 
 /**
  * obtiene el clima actual y el pronostico de 7 dias para una ciudad especifica
